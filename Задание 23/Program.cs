152177c572d50bc016818003d4bbece8fe831570
Анна Белозерова <bel.denisovna@gmail.com>
2021-11-06 17:20:40 +0300

    Добавьте файлы проекта.

---
+++ Задание 23/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Задание_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Factorial1Async(10);
+                Factorial2(8);
+
+            Console.WriteLine("Main окончил работу.");
+            Console.ReadKey();
+
+        }
+        static void Factorial1(uint n)
+        {
+            ulong f=1;
+            for (uint i = 2; i <= n; i++)
+            {
+                f *= i;
+                Console.WriteLine("Factorial 1: {0}",f);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Factorial 1: Факториал числа равен: {0}", f);
+        }
+        static void Factorial2(uint n)
+        {
+            ulong f = 1;
+            for (uint i = 2; i <= n; i++)
+            {
+                f *= i;
+                Console.WriteLine("Factorial 2: {0}",f);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Factorial 2: Факториал числа равен: {0}", f);
+        }
+        static async void Factorial1Async (uint n)
+        {
+            Console.WriteLine("FactorialAsync начал работу");
+            await Task.Run(() => Factorial1(n));
+            Console.WriteLine("FactorialAsync закончил работу");
+        }
+    }
+}
